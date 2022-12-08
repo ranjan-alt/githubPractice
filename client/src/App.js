e157f5a315479c4706972bdbbcd9752714ae44f8
Ranjan Kumar <67799181+ranjan-alt@users.noreply.github.com>
2022-12-08 12:16:06 +0000

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,46 @@
-import "./App.css";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Outlet,
-} from "react-router-dom";
-import Products from "./pages/Products/Products";
-import Product from "./pages/Product/Product";
-import Home from "./pages/Home/Home";
-import Navbar from "./component/Navbar/Navbar";
-import Footer from "./component/Footer/Footer";
 import { Children } from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
+// import "./app.scss";
 
 const Layout = () => {
   return (
-    <>
-      <div>
-        <Navbar />
-        <Outlet />
-        <Footer />
-      </div>
-    </>
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
   );
 };
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      Children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/products/:id",
-          element: <Products />,
-        },
-        {
-          path: "/product/:id",
-          element: <Product />,
-        },
-      ],
-    },
-  ]);
   return (
-    <div className="App">
+    <div>
       <RouterProvider router={router} />
     </div>
   );
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Slider from "../../components/Slider/Slider";
 const Home = () => {
-  return <>home</>;
+  return (
+    <>
+      <Slider />
+    </>
+  );
 };
 
 export default Home;
